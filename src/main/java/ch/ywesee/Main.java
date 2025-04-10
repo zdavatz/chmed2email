086b704da568d7e0c8f7f8509c1e60fa03783221
b123400 <i@b123400.net>
2025-04-10 21:43:14 +0900

    Take PDF and extract all images

--- src/main/java/ch/ywesee/Main.java
+++ src/main/java/ch/ywesee/Main.java
@@ -9,6 +9,13 @@ import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.*;
 import org.apache.commons.cli.*;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -22,6 +29,9 @@ public class Main {
 
         Option qrImage = new Option("q", "qr-code", true, "Path to QRCode");
         options.addOption(qrImage);
+
+        Option pdfOption = new Option("p", "pdf", true, "Path to PDF file");
+        options.addOption(pdfOption);
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -47,7 +57,7 @@ public class Main {
 
         String qrCodeImagePath = cmd.getOptionValue("qr-code");
         if (qrCodeImagePath != null) {
-            String content = scanQRImage(qrCodeImagePath);
+            String content = scanQRImage(ImageIO.read(new File(qrCodeImagePath)));
             if (content != null) {
                 System.out.println("Content in QRCode:" + content);
                 EPrescription ep = new EPrescription(content);
@@ -56,10 +66,39 @@ public class Main {
             }
         }
 
-    public static String scanQRImage(String path) throws IOException {
+
+        String pdfPath = cmd.getOptionValue("pdf");
+        if (pdfPath != null) {
+            PDDocument document = Loader.loadPDF(new File(pdfPath));
+            int imageCountInPDF = 0;
+            boolean foundQRCodeInPDF = false;
+            for (PDPage page : document.getPages()) {
+                PDResources resources = page.getResources();
+                for (COSName name : resources.getXObjectNames()) {
+                    PDXObject obj = resources.getXObject(name);
+                    if (obj instanceof PDImageXObject) {
+                        imageCountInPDF++;
+                        BufferedImage image = ((PDImageXObject) obj).getImage();
+                        String result = scanQRImage(image);
+                        if (result != null) {
+                            System.out.println("Found QRCode in PDF " + result);
+                            foundQRCodeInPDF = true;
+                            EPrescription ep = new EPrescription(result);
+                            ZurRosePrescription zp = ep.toZurRosePrescription();
+                            System.out.println(zp.toXML());
+                        }
+                    }
+                }
+            }
+            if (imageCountInPDF > 0 && !foundQRCodeInPDF) {
+                System.err.println(imageCountInPDF + " images found in PDF, but no QRCode is found");
+            }
+        }
+    }
+
+    public static String scanQRImage(BufferedImage bufferedImage) throws IOException {
         String contents = null;
 
-        BufferedImage bufferedImage = ImageIO.read(new File(path));
         BufferedImageLuminanceSource bufferedImageLuminanceSource = new BufferedImageLuminanceSource(bufferedImage);
         BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(bufferedImageLuminanceSource));
 
@@ -69,7 +108,7 @@ public class Main {
             contents = result.getText();
         }
         catch (Exception e) {
-            System.err.println("Error decoding barcode " + e);
+//            System.err.println("Error decoding barcode " + e);
         }
         return contents;
     }
